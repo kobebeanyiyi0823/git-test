a94c5b78f016037c3a4c81bb2cde3ea5f735b6e6
liuyiyi <bigdata@atguigu.com>
2021-06-28 08:26:34 +0800

    4

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("aaa");
+        System.out.println("hot-fix");
+
     }
 }
